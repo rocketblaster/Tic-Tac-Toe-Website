ab52441140a164ba854a3ceb1a73d2f9b94b21fa
Jenny Cao <jennycamcao@gmail.com>
2026-06-02 21:27:28 -0700

    Add win states of an array

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ endMessage.textContent = `X's turn`
 endMessage.style.marginTop = '30px'
 endMessage.style.textAlign = 'center'
 board.after(endMessage)
+
+const winning_combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
