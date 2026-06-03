215f8fc6206b6f4575c699842a7a9f259c4222e2
Jenny Cao <jennycamcao@gmail.com>
2026-06-02 21:26:09 -0700

    Add end message

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const board = document.getElementbyId('container')
 const squares = document.getElementByClassName('cell')
 const players = ['X', 'O']
 let currentPlayer = players[0]
+
+const endMessage = document.createElement('p')
+endMessage.textContent = `X's turn`
+endMessage.style.marginTop = '30px'
+endMessage.style.textAlign = 'center'
+board.after(endMessage)
